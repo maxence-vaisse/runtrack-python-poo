00839b31c17be48694dfcdfbfbbe256930e8c66e
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-01-03 09:48:30 +0100

    Runtrack Jour 02 Job02

---
+++ jour02/job02/job02.py
@@ -0,0 +1,38 @@
+class Livre: # Création de la classe Livre
+    def __init__(self, titre, auteur, nbpages): # Création de la méthode __init__ avec les attributs titre, auteur et nbpages
+        self._titre = titre # Création de l'attribut titre
+        self._auteur = auteur # Création de l'attribut auteur
+        self._nbpages = nbpages # Création de l'attribut nbpages
+
+    def __setTitre__(self, titre): # Création de la méthode __setTitre__ avec l'attribut titre
+        self._titre = titre # Création de l'attribut titre
+
+    def __getTitre__(self): # Création de la méthode __getTitre__
+        return self._titre # Retourne l'attribut titre
+    
+    def __setAuteur__(self, auteur): # Création de la méthode __setAuteur__ avec l'attribut auteur
+        self._auteur = auteur # Création de l'attribut auteur
+
+    def __getAuteur__(self): # Création de la méthode __getAuteur__
+        return self._auteur # Retourne l'attribut auteur
+    
+    def __setNbpages__(self, nbpages): # Création de la méthode __setNbpages__ avec l'attribut nbpages
+        if int == type(nbpages) and nbpages > 0:
+            self._nbpages = nbpages
+        else:
+            print("Le nombre de pages doit être un entier.")
+            exit(1)
+
+    def __getNbpages__(self): # Création de la méthode __getNbpages__
+        return self._nbpages # Retourne l'attribut nbpages
+    
+Livre1 = Livre("Harry Potter", "J.K. Rowling", 300) # Création de l'objet Livre1 avec les attributs titre, auteur et nbpages
+print(Livre1.__getTitre__()) # Affiche l'attribut titre
+print(Livre1.__getAuteur__()) # Affiche l'attribut auteur
+print(Livre1.__getNbpages__()) # Affiche l'attribut nbpages
+(Livre1.__setTitre__("Le Seigneur des Anneaux")) # Modifie l'attribut titre
+(Livre1.__setAuteur__("J.R.R. Tolkien")) # Modifie l'attribut auteur
+(Livre1.__setNbpages__(250)) # Modifie l'attribut nbpages
+print(Livre1.__getTitre__()) # Affiche l'attribut titre
+print(Livre1.__getAuteur__()) # Affiche l'attribut auteur
+print(Livre1.__getNbpages__()) # Affiche l'attribut nbpages
