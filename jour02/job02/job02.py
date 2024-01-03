804c75070184e9dff35b73032de339583637b470
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-01-03 14:59:43 +0100

    Runtrack Jour 02 Job02

--- jour02/job02/job02.py
+++ jour02/job02/job02.py
@@ -4,35 +4,35 @@ class Livre: # Création de la classe Livre
         self.__auteur = auteur # Création de l'attribut auteur
         self.__nbpages = nbpages # Création de l'attribut nbpages
 
-    def __setTitre__(self, titre): # Création de la méthode __setTitre__ avec l'attribut titre
+    def _setTitre(self, titre): # Création de la méthode __setTitre__ avec l'attribut titre
         self.__titre = titre # Création de l'attribut titre
 
-    def __getTitre__(self): # Création de la méthode __getTitre__
+    def _getTitre(self): # Création de la méthode __getTitre__
         return self.__titre # Retourne l'attribut titre
     
-    def __setAuteur__(self, auteur): # Création de la méthode __setAuteur__ avec l'attribut auteur
+    def _setAuteur(self, auteur): # Création de la méthode __setAuteur__ avec l'attribut auteur
         self.__auteur = auteur # Création de l'attribut auteur
 
-    def __getAuteur__(self): # Création de la méthode __getAuteur__
+    def _getAuteur(self): # Création de la méthode __getAuteur__
         return self.__auteur # Retourne l'attribut auteur
     
-    def __setNbpages__(self, nbpages): # Création de la méthode __setNbpages__ avec l'attribut nbpages
+    def _setNbpages(self, nbpages): # Création de la méthode __setNbpages__ avec l'attribut nbpages
         if int == type(nbpages) and nbpages > 0:
             self.__nbpages = nbpages
         else:
             print("Le nombre de pages doit être un entier.")
             exit(1)
 
-    def __getNbpages__(self): # Création de la méthode __getNbpages__
+    def _getNbpages(self): # Création de la méthode __getNbpages__
         return self.__nbpages # Retourne l'attribut nbpages
     
 Livre1 = Livre("Harry Potter", "J.K. Rowling", 300) # Création de l'objet Livre1 avec les attributs titre, auteur et nbpages
-print(Livre1.__getTitre__()) # Affiche l'attribut titre
-print(Livre1.__getAuteur__()) # Affiche l'attribut auteur
-print(Livre1.__getNbpages__()) # Affiche l'attribut nbpages
-(Livre1.__setTitre__("Le Seigneur des Anneaux")) # Modifie l'attribut titre
-(Livre1.__setAuteur__("J.R.R. Tolkien")) # Modifie l'attribut auteur
-(Livre1.__setNbpages__(250)) # Modifie l'attribut nbpages
-print(Livre1.__getTitre__()) # Affiche l'attribut titre
-print(Livre1.__getAuteur__()) # Affiche l'attribut auteur
-print(Livre1.__getNbpages__()) # Affiche l'attribut nbpages
+print(Livre1._getTitre()) # Affiche l'attribut titre
+print(Livre1._getAuteur()) # Affiche l'attribut auteur
+print(Livre1._getNbpages()) # Affiche l'attribut nbpages
+(Livre1._setTitre("Le Seigneur des Anneaux")) # Modifie l'attribut titre
+(Livre1._setAuteur("J.R.R. Tolkien")) # Modifie l'attribut auteur
+(Livre1._setNbpages(250)) # Modifie l'attribut nbpages
+print(Livre1._getTitre()) # Affiche l'attribut titre
+print(Livre1._getAuteur()) # Affiche l'attribut auteur
+print(Livre1._getNbpages()) # Affiche l'attribut nbpages
