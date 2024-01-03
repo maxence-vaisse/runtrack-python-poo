cef7964d386546f81440fb669ac25deb1e94c83f
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-01-03 10:40:18 +0100

    Runtrack Jour 02 Job01

--- jour02/job01/job01.py
+++ jour02/job01/job01.py
@@ -1,19 +1,19 @@
 class Rectangle: # Création de la classe Rectangle
     def __init__(self, longeur, largeur): # Création de la méthode __init__ avec les attributs longeur et largeur
-        self._longeur = longeur # Création de l'attribut longeur
-        self._largeur = largeur # Création de l'attribut largeur
+        self.__longeur = longeur # Création de l'attribut longeur
+        self.__largeur = largeur # Création de l'attribut largeur
 
     def __setLongeur__(self, longeur): # Création de la méthode __setLongeur__ avec l'attribut longeur
-        self._longeur = longeur # Création de l'attribut longeur
+        self.__longeur = longeur # Création de l'attribut longeur
 
     def __getLongeur__(self): # Création de la méthode __getLongeur__
-        return self._longeur # Retourne l'attribut longeur
+        return self.__longeur # Retourne l'attribut longeur
     
     def __setLargeur__(self, largeur): # Création de la méthode __setLargeur__ avec l'attribut largeur
-        self._largeur = largeur # Création de l'attribut largeur
+        self.__largeur = largeur # Création de l'attribut largeur
 
     def __getLargeur__(self): # Création de la méthode __getLargeur__
-        return self._largeur # Retourne l'attribut largeur
+        return self.__largeur # Retourne l'attribut largeur
     
 Rectangle1 = Rectangle(10, 5) # Création de l'objet Rectangle1 avec les attributs longeur et largeur
 print(Rectangle1.__getLongeur__()) # Affiche l'attribut longeur
