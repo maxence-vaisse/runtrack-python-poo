921b5f4a7a46ffd0f63524a14395bf10e1332154
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-01-03 09:30:41 +0100

    Runtrack Jour 02 Job01

---
+++ jour02/job01/job01.py
@@ -0,0 +1,24 @@
+class Rectangle: # Création de la classe Rectangle
+    def __init__(self, longeur, largeur): # Création de la méthode __init__ avec les attributs longeur et largeur
+        self._longeur = longeur # Création de l'attribut longeur
+        self._largeur = largeur # Création de l'attribut largeur
+
+    def __setLongeur__(self, longeur): # Création de la méthode __setLongeur__ avec l'attribut longeur
+        self._longeur = longeur # Création de l'attribut longeur
+
+    def __getLongeur__(self): # Création de la méthode __getLongeur__
+        return self._longeur # Retourne l'attribut longeur
+    
+    def __setLargeur__(self, largeur): # Création de la méthode __setLargeur__ avec l'attribut largeur
+        self._largeur = largeur # Création de l'attribut largeur
+
+    def __getLargeur__(self): # Création de la méthode __getLargeur__
+        return self._largeur # Retourne l'attribut largeur
+    
+Rectangle1 = Rectangle(10, 5) # Création de l'objet Rectangle1 avec les attributs longeur et largeur
+print(Rectangle1.__getLongeur__()) # Affiche l'attribut longeur
+print(Rectangle1.__getLargeur__()) # Affiche l'attribut largeur
+(Rectangle1.__setLongeur__(7)) # Modifie l'attribut longeur
+(Rectangle1.__setLargeur__(3)) # Modifie l'attribut largeur
+print(Rectangle1.__getLongeur__()) # Affiche l'attribut longeur
+print(Rectangle1.__getLargeur__()) # Affiche l'attribut largeur
