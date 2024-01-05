2152ad7002ec3d9545844eb05fc35523c293ecc7
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-01-05 11:43:14 +0100

    Runtrack Jour 04 Job04

--- jour04/job04/job04.py
+++ jour04/job04/job04.py
@@ -1,14 +1,29 @@
 class Forme:
     def aire(self):
-        return 0
-    
+        self.__aire = 0
+        return self.__aire
+
 class Rectangle(Forme):
-    def __init__(self, longueur, largeur):
+    def __init__(self, longueur, hauteur):
         self.__longueur = longueur
-        self.__largeur = largeur
-        
+        self.__hauteur = hauteur
+
     def aire(self):
-        return self.__longueur * self.__largeur
+        aire = self.__hauteur * self.__longueur
+        return aire
     
-rectangle = Rectangle(7, 10)
-print(rectangle.aire())
+    def getLongueur(self):
+        return self.__longueur
+    
+    def getHauteur(self):
+        return self.__hauteur
+    
+    def setLongueur(self, longueur):
+        self.__longueur = longueur
+
+    def setHauteur(self, hauteur):
+        self.__hauteur = hauteur
+
+rectangle = Rectangle(5, 9)
+
+print("L'aire du rectangle est :", rectangle.aire())
